90ec821ca9ef98ebc2df572dc1ee5490838b4688
Anna Swan <42541919+annaswan0001@users.noreply.github.com>
2020-06-08 11:41:25 +0300

    Updates

---
+++ precache-manifest.9bc2d7a003823a11319ee887c6042af9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ecb917c55ee99ddc388ed95f1f7898a",
+    "url": "/react-table/index.html"
+  },
+  {
+    "revision": "5fee104182b7a3034f9a",
+    "url": "/react-table/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6c520641a82a2b0e13bd",
+    "url": "/react-table/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5fee104182b7a3034f9a",
+    "url": "/react-table/static/js/2.176eccfd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-table/static/js/2.176eccfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c520641a82a2b0e13bd",
+    "url": "/react-table/static/js/main.bd4f7c89.chunk.js"
+  },
+  {
+    "revision": "709eb39da638b44947b2",
+    "url": "/react-table/static/js/runtime-main.9e0688f3.js"
+  }
+]);
